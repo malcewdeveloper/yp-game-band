c0a11185fb8a15ed7acc751c5eb86e8815cc7770
Svetlana Prozhirova <svetakesha@yandex.ru>
2024-08-01 16:26:11 +0300

    feat: экран завершения игры

--- packages/client/src/pages/Game/index.tsx
+++ packages/client/src/pages/Game/index.tsx
@@ -1,6 +1,10 @@
 import React, { useState, useEffect } from "react";
 import { Progress, Button, Tooltip } from "antd";
-import { InfoCircleOutlined } from "@ant-design/icons";
+import {
+    InfoCircleOutlined,
+    StarTwoTone,
+    SyncOutlined,
+} from "@ant-design/icons";
 
 const GameStart: React.FC<object> = () => {
     const [percent, setpercent] = useState(0);
@@ -48,6 +52,61 @@ const GameStart: React.FC<object> = () => {
     );
 };
 
+const GameEndWin: React.FC<object> = () => {
+    return (
+        <div
+            style={{
+                boxShadow: "0px 0px 12px #444",
+                width: "400px",
+                height: "200px",
+                display: "flex",
+                margin: "0 auto",
+                flexDirection: "column",
+            }}
+        >
+            <div style={{ margin: "5px auto" }}>
+                <StarTwoTone />
+                <StarTwoTone />
+            </div>
+            <p style={{ margin: "5px auto" }}>1-13</p>
+            <h2 style={{ margin: "5px auto" }}>Уровень пройден</h2>
+            <h1 style={{ margin: "5px auto" }}>Отлично сработано!</h1>
+            <div
+                style={{
+                    margin: "5px auto",
+                    gap: "10px",
+                    display: "flex",
+                    flexDirection: "row",
+                }}
+            >
+                <SyncOutlined />
+                <Button
+                    type="primary"
+                    style={{
+                        width: "100px",
+                        margin: "0 auto",
+                    }}
+                >
+                    Продолжить
+                </Button>
+                <Button
+                    type="primary"
+                    style={{
+                        width: "100px",
+                        margin: "0 auto",
+                    }}
+                >
+                    Выход из игры
+                </Button>
+            </div>
+        </div>
+    );
+};
+
+const GameEndLose: React.FC<object> = () => {
+    return <div>2</div>;
+};
+
 export const Game: React.FC<object> = () => {
-    return <GameStart />;
+    return <GameEndWin />;
 };
